9e174a3d239cdcf06685a60034da5edf5e2464a3
tohodson <tohodson@gmail.com>
2014-02-10 12:04:43 -0600

    initial bag_merge working

--- bag_merge.sh
+++ bag_merge.sh
@@ -1,10 +1,13 @@
 #/bin/bash
-
+################################################################################
+# AUTHOR: TO Hodson 
+# simple wrapper for gdal_merge, which merges all resampled rasters in CWD
+################################################################################
 
 NAME=$1
 
-FILE_LIST=$(ls | grep resampled | grep -v Combined | sort -n -t _ -k 4)
+FILE_LIST=$(ls | grep resampled | grep tiff$ | sort -n -t _ -k 4)
 
-gdal_merge.py -of GTiff -o $NAME.tiff $FILE_LIST
+gdal_merge.py -v -n 0 -of GTiff -o $NAME.tiff $FILE_LIST
 
-rm *resampled*
+#rm *resampled*
